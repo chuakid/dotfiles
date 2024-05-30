49a8abc56caa5bd4e9bef5af8d2be7fe6bd35b28
Kelvin <chuakid@gmail.com>
2024-05-30 10:47:09 +0800

    stow all command

--- stow_all.sh
+++ stow_all.sh
@@ -1,12 +1,35 @@
 #!/usr/bin/env bash
-for folder in */;
-do
-    echo "Install ${folder::-1}?" # remove / from folder name
-    select choice in "Yes" "No"; do
-        case $choice in
-            Yes) stow --dotfiles "${folder}"; break;;
-            No) break;;
-        esac
-    done
+
+# Colors
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+BLUE='\033[0;34m'
+
+
+echo -e "${RED}all?${NC}"
+select choice in "Yes" "No"; do
+    case $choice in
+    Yes)
+        for folder in */; do stow --dotfiles "${folder}"; done
+        exit
+        ;;
+    No) break ;;
+    esac
 done
 
+echo
+
+for folder in */; do
+    echo -e "${BLUE}${folder::-1}?${NC}" # remove / from folder name
+    select choice in "Yes" "No"; do
+        case $choice in
+        Yes)
+            echo "Stowing ${folder}"
+            stow --dotfiles "${folder}"
+            break
+            ;;
+        No) break ;;
+        esac
+    done
+    echo
+done
