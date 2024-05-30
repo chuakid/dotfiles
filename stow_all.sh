25b35fadaf82f3f9fd613c89d9db67fd94f3f15c
Kelvin <chuakid@gmail.com>
2024-05-30 10:27:19 +0800

    add menu to choose folders

--- stow_all.sh
+++ stow_all.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 for folder in */;
 do
-    stow --dotfiles ${folder}
+    echo "Install ${folder::-1}?" # remove / from folder name
+    select choice in "Yes" "No"; do
+        case $choice in
+            Yes) stow --dotfiles "${folder}"; break;;
+            No) break;;
+        esac
+    done
 done
 
