62c0429755ed7e1eed8fb7aefcf8e8942775e9de
Kelvin <chuakid@gmail.com>
2024-06-18 22:30:56 +0800

    installation scripts

---
+++ fedora_home_install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# install everything
+sudo dnf install $(< packages.list)
+
+# Install omz
+if [ -d ~/.oh-my-zsh ]; then
+    echo "omz already installed"
+else
+    /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/HEAD/tools/install.sh)"
+fi
+
+# install faster-node-manager (fnm)
+curl -fsSL https://fnm.vercel.app/install | bash -s -- --skip-shell
+
+# install pyenv
+curl https://pyenv.run | bash
--- work_install.sh
+++ work_install.sh
@@ -1,9 +1,3 @@
-# Install zsh
-if [ ! $(which zsh) ]; then
-    echo "install zsh first"
-    exit
-fi
-
 # Install omz
 if [ -d ~/.oh-my-zsh ]; then
     echo "omz already installed"
