762e4237c7e1f67557ce1017ce7517cb4cf341cc
Your Name <you@example.com>
2024-11-27 23:53:57 +0800

    fix install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
-git submodule update --init --recursive
-
 # Install omz
 /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/HEAD/tools/install.sh)"
 
+# stow
+stow common --adopt
+git reset --hard # to reset zshrc
