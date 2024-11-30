3fe883cc55476ca194703ec7d300d679f8b55022
Kelvin <chuakid@gmail.com>
2024-11-30 20:29:26 +0800

    update install

--- install2.sh
+++ install2.sh
@@ -1,7 +1,3 @@
-# Install omz
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-# Install nix
-curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install 
 # Install home manager
 nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
 nix-channel --update
