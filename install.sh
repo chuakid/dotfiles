18fbdfdbda4be7c20beb0018a889c564bf4eb926
Kelvin <chuakid@gmail.com>
2024-11-30 18:47:16 +0800

    no start daemon false

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 # Install omz
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 # Install nix
-curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install
+curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install --no-start-daemon false
 # Install home manager
 nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
 nix-channel --update
