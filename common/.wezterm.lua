0b627a182871e29c1a2588f3ad951285aac803bb
Kelvin <chuakid@gmail.com>
2025-02-04 16:25:20 +0800

    wezterm config

--- common/.wezterm.lua
+++ common/.wezterm.lua
@@ -1,14 +1,10 @@
 -- Pull in the wezterm API
 local wezterm = require 'wezterm'
-
--- This will hold the configuration.
 local config = wezterm.config_builder()
+config.color_scheme = 'Catppuccin Macchiato'
+config.use_fancy_tab_bar = false
+config.tab_bar_at_bottom = true
+config.hide_tab_bar_if_only_one_tab = true 
 
--- This is where you actually apply your config choices
-
--- For example, changing the color scheme:
-config.color_scheme = 'catppuccin-macchiato'
-
--- and finally, return the configuration to wezterm
 return config
 
