bf7c80dbff01b6c9fbf95522461e97a5dc19dea9
Kelvin <chuakid@gmail.com>
2025-02-04 19:35:25 +0800

    remove padding

--- common/.wezterm.lua
+++ common/.wezterm.lua
@@ -1,10 +1,18 @@
 -- Pull in the wezterm API
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
+
 config.color_scheme = 'Catppuccin Macchiato'
 config.use_fancy_tab_bar = false
 config.tab_bar_at_bottom = true
 config.hide_tab_bar_if_only_one_tab = true 
+
+config.window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0
+}
 
 return config
 
