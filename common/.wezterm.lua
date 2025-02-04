c29da9e719c216e9685b347ae4b886701f3795de
Kelvin <chuakid@gmail.com>
2025-02-04 21:11:40 +0800

    keys for wezterm

--- common/.wezterm.lua
+++ common/.wezterm.lua
@@ -1,7 +1,8 @@
--- Pull in the wezterm API
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
+local act = wezterm.action
 
+-- styling
 config.color_scheme = 'Catppuccin Macchiato'
 config.use_fancy_tab_bar = false
 config.tab_bar_at_bottom = true
@@ -13,6 +14,42 @@ config.window_padding = {
     top = 0,
     bottom = 0
 }
+
+-- keys
+config.leader = { key = 'b', mods = 'CTRL', timeout_milliseconds = 2000}
+config.keys = {
+    {
+        key = '-',
+        mods = 'LEADER',
+        action = act.SplitVertical,
+    },
+    {
+        key = '|',
+        mods = 'LEADER|SHIFT',
+        action = act.SplitHorizontal,
+    },
+    {
+        key = 't',
+        mods = 'LEADER',
+        action = act.SpawnTab 'CurrentPaneDomain',
+    }, 
+    {
+        key = 'x',
+        mods = 'LEADER',
+        action = act.CloseCurrentPane { confirm = true },
+    },
+    {
+        key = '[',
+        mods = 'LEADER',
+        action = act.ActivateCopyMode
+    },
+    {
+        key = 'w',
+        mods = 'LEADER',
+        action = act.ShowTabNavigator 
+    }
+}
+
 
 return config
 
