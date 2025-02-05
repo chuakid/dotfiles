632d3bf5ab8945abc4e039fa7bd094d57eaf94ad
Kelvin <chuakid@gmail.com>
2025-02-05 12:05:39 +0800

    add back tab bar

--- common/.config/wezterm/wezterm.lua
+++ common/.config/wezterm/wezterm.lua
@@ -6,7 +6,6 @@ local act = wezterm.action
 config.color_scheme = 'Catppuccin Macchiato'
 config.use_fancy_tab_bar = false
 config.tab_bar_at_bottom = true
-config.hide_tab_bar_if_only_one_tab = true 
 
 config.window_padding = {
     left = 0,
