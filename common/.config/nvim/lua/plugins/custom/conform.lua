ffe42e0b5bfef4570a818aedf7fc736983769a0f
Kelvin <chuakid@gmail.com>
2025-03-15 23:00:14 +0800

    formatting and eslint

--- common/.config/nvim/lua/plugins/custom/conform.lua
+++ common/.config/nvim/lua/plugins/custom/conform.lua
@@ -13,27 +13,16 @@ return { -- Autoformat
     },
   },
   opts = {
-    notify_on_error = false,
-    format_on_save = function(bufnr)
-      -- Disable "format_on_save lsp_fallback" for languages that don't
-      -- have a well standardized coding style. You can add additional
-      -- languages here or re-enable it for the disabled ones.
-      local disable_filetypes = { c = true, cpp = true }
-      local lsp_format_opt
-      if disable_filetypes[vim.bo[bufnr].filetype] then
-        lsp_format_opt = 'never'
-      else
-        lsp_format_opt = 'fallback'
-      end
-      return {
-        timeout_ms = 500,
-        lsp_format = lsp_format_opt,
-      }
-    end,
+    format_on_save = { lsp_fallback = true, async = false },
     formatters_by_ft = {
       lua = { 'stylua' },
       python = { 'isort', 'black' },
       javascript = { 'prettierd', 'prettier', stop_after_first = true },
+      typescript = { 'prettierd', 'prettier', stop_after_first = true },
+      javascriptreact = { 'prettierd', 'prettier', stop_after_first = true },
+      typescriptreact = { 'prettierd', 'prettier', stop_after_first = true },
+      css = { 'prettierd', 'prettier', stop_after_first = true },
+      html = { 'prettierd', 'prettier', stop_after_first = true },
       go = { 'gofmt' },
     },
   },
