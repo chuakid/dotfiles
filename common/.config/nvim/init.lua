cad3c19fbc0988366b671750b8f20673821cb956
Kelvin <chuakid@gmail.com>
2025-03-19 10:52:41 +0800

    transparent bg

--- common/.config/nvim/init.lua
+++ common/.config/nvim/init.lua
@@ -158,7 +158,6 @@ require('lazy').setup({
     config = function()
       require('catppuccin').setup {
         flavour = 'macchiato',
-        transparent_background = true,
       }
       vim.cmd.colorscheme 'catppuccin'
     end,
