43faca60e91aa5a38bbf731cd3b2ccc0c5886487
Kelvin <chuakid@gmail.com>
2025-03-30 02:11:46 +0800

    move to stable neovim

--- common/.config/home-manager/flake.nix
+++ common/.config/home-manager/flake.nix
@@ -8,15 +8,16 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
+    # enable to install nightly neovim
+    # neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
   };
 
-  outputs = { nixpkgs, home-manager, neovim-nightly-overlay, ... }: {
+  outputs = { nixpkgs, home-manager, ... }: {
     homeConfigurations = {
       kelvin = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.x86_64-linux;
         modules = [
-          { nixpkgs.overlays = [ neovim-nightly-overlay.overlays.default ]; }
+          # { nixpkgs.overlays = [ neovim-nightly-overlay.overlays.default ]; }
           ./home.nix
         ];
         extraSpecialArgs = {
@@ -27,7 +28,7 @@
       SP13304 = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.aarch64-darwin;
         modules = [
-          { nixpkgs.overlays = [ neovim-nightly-overlay.overlays.default ]; }
+          # { nixpkgs.overlays = [ neovim-nightly-overlay.overlays.default ]; }
           ./work.nix
         ];
         extraSpecialArgs = {
