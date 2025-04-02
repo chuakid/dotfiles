86f5b31dea310f5625ded132e8713a204859ee9d
Kelvin <chuakid@gmail.com>
2025-04-02 20:05:47 +0800

    fix lint

--- common/.config/nvim/lua/plugins/lint.lua
+++ common/.config/nvim/lua/plugins/lint.lua
@@ -14,18 +14,15 @@ return {
       }
       -- HACK: temp fix until
       -- https://github.com/mfussenegger/nvim-lint/pull/761 is merged
-      lint.linters.golangcilint.args = (function()
-        if string.find(vim.fn.system { 'golangci-lint', 'version' }, 'version v2') then
-          return {
-            'run',
-            '--output.json.path=stdout',
-            '--issues-exit-code=0',
-            '--show-stats=false',
-            function()
-              return vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ':h')
-            end,
-          }
-        else
+      local getArgs = function()
+        local ok, output = pcall(vim.fn.system, { 'golangci-lint', 'version' })
+        if not ok then
+          return
+        end
+
+        -- The golangci-lint install script and prebuilt binaries strip the v from the version
+        --   tag so both strings must be checked
+        if string.find(output, 'version v1') or string.find(output, 'version 1') then
           return {
             'run',
             '--out-format',
@@ -39,8 +36,19 @@ return {
             end,
           }
         end
-      end)()
 
+        return {
+          'run',
+          '--output.json.path=stdout',
+          '--issues-exit-code=0',
+          '--show-stats=false',
+          function()
+            return vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ':h')
+          end,
+        }
+      end
+
+      lint.linters.golangcilint.args = getArgs()
       -- Create autocommand which carries out the actual linting
       -- on the specified events.
       local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
