2dd3a60e0595511943b46a1b9ae854a770d2c5ac
Kelvin <chuakid@gmail.com>
2025-05-19 13:05:33 +0800

    update and add noice

--- common/.config/nvim/lua/plugins/misc.lua
+++ common/.config/nvim/lua/plugins/misc.lua
@@ -15,4 +15,15 @@ return {
       default_mappings = true,
     },
   },
+  {
+    'folke/noice.nvim',
+    event = 'VeryLazy',
+    opts = {
+      -- add any options here
+    },
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      'MunifTanjim/nui.nvim',
+    },
+  },
 }
