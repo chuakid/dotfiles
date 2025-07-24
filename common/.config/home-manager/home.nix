29a5711d47e6fea57e56dc99552cb24e2ac9806f
Kelvin <chuakid@gmail.com>
2025-07-24 21:15:26 +0800

    add typst

--- common/.config/home-manager/home.nix
+++ common/.config/home-manager/home.nix
@@ -5,7 +5,7 @@
   home.username = username;
   home.homeDirectory = homeDirectory;
 
-  home.packages = with pkgs; [ wl-clipboard ];
+  home.packages = with pkgs; [ wl-clipboard typst ];
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
