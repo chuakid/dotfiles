309ba3e29a545b2673e88e07419c90fe0e351c02
Kelvin <chuakid@gmail.com>
2025-07-25 15:35:04 +0800

    remove comments

--- common/.config/wezterm/wezterm.lua
+++ common/.config/wezterm/wezterm.lua
@@ -67,12 +67,7 @@ config.keys = {
 }
 
 local smart_splits = wezterm.plugin.require("https://github.com/mrjones2014/smart-splits.nvim")
--- you can put the rest of your Wezterm config here
 smart_splits.apply_to_config(config, {
-	-- the default config is here, if you'd like to use the default keys,
-	-- you can omit this configuration table parameter and just use
-	-- smart_splits.apply_to_config(config)
-
 	-- directional keys to use in order of: left, down, up, right
 	direction_keys = { "h", "j", "k", "l" },
 	-- modifier keys to combine with direction_keys
