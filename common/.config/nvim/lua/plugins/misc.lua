ad074edad4028b83c7f8eed1230a5099a6208baa
Kelvin <chuakid@gmail.com>
2025-07-27 23:29:31 +0800

    move spectre into misc

--- common/.config/nvim/lua/plugins/misc.lua
+++ common/.config/nvim/lua/plugins/misc.lua
@@ -71,4 +71,20 @@ return {
       },
     },
   },
+  {
+    'nvim-pack/nvim-spectre',
+    keys = {
+      {
+        '<leader>sx',
+        function()
+          require('spectre').toggle()
+        end,
+        desc = 'Toggle spectre',
+      },
+    },
+    opts = {},
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+  },
 }
