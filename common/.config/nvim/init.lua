7db2f6f28667780cd666c38e80974cd933db24c1
Kelvin <chuakid@gmail.com>
2025-07-27 23:30:43 +0800

    remove: comments

--- common/.config/nvim/init.lua
+++ common/.config/nvim/init.lua
@@ -99,13 +99,6 @@ vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show error
 -- Exit terminal mode with double esc
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
--- Keybinds to make split navigation easier.
---  Use CTRL+<hjkl> to switch between windows
--- vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
--- vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
--- vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
--- vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
-
 vim.keymap.set('n', 'H', '^', { desc = 'Go to start of line' })
 vim.keymap.set('n', 'L', '$', { desc = 'Go to end of line' })
 
