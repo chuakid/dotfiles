fc13eafff967031d05b2eb510a1967953dc49846
Kelvin <chuakid@gmail.com>
2025-08-12 00:32:24 +0800

    add no confirm, remove ubuntu script

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 # Install nix
-curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install
+curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install --no-confirm
 source /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
 
 nix run home-manager/master switch
