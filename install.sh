a9e5cd27cf722dfcfaedcf9c29de6842d3f27392
Kelvin <chuakid@gmail.com>
2025-08-12 00:29:40 +0800

    fix install script

--- install.sh
+++ install.sh
@@ -1,6 +1,4 @@
-nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
-nix-channel --update
-nix-shell '<home-manager>' -A install
+nix run home-manager/master switch
 
 # stow
 stow common --adopt
