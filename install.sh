fa655e4a57a7394727764b52d7570b1fd3f7b3c6
Kelvin <chuakid@gmail.com>
2025-08-12 16:30:49 +0800

    nix channels

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 # Install nix
 curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install --no-confirm
+
 source /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+
+nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+nix-channel --update
 
 nix run home-manager/master switch
 
