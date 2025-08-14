ca6d18dcfd3a2a82594eaa4ae3326246de2ca2b6
Kelvin <chuakid@gmail.com>
2025-08-14 12:28:35 -0400

    fedora script

---
+++ fedora_script.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+# "Things To Do!" script for a fresh Fedora installation
+# From https://nattdf.streamlit.app/
+
+# Check if the script is run with sudo
+if [ "$EUID" -ne 0 ]; then
+    echo "Please run this script with sudo"
+    exit 1
+fi
+
+# Funtion to echo colored text
+color_echo() {
+    local color="$1"
+    local text="$2"
+    case "$color" in
+        "red")     echo -e "\033[0;31m$text\033[0m" ;;
+        "green")   echo -e "\033[0;32m$text\033[0m" ;;
+        "yellow")  echo -e "\033[1;33m$text\033[0m" ;;
+        "blue")    echo -e "\033[0;34m$text\033[0m" ;;
+        *)         echo "$text" ;;
+    esac
+}
+
+# Set variables
+ACTUAL_USER=$SUDO_USER
+ACTUAL_HOME=$(eval echo ~$SUDO_USER)
+INITIAL_DIR=$(pwd)
+
+# Function to generate timestamps
+get_timestamp() {
+    date +"%Y-%m-%d %H:%M:%S"
+}
+
+# Function to log messages
+log_message() {
+    local message="$1"
+    echo "$(get_timestamp) - $message"
+}
+
+# Function to handle errors
+handle_error() {
+    local exit_code=$?
+    local message="$1"
+    if [ $exit_code -ne 0 ]; then
+        color_echo "red" "ERROR: $message"
+        exit $exit_code
+    fi
+}
+
+# Replace Fedora Flatpak Repo with Flathub for better package management and apps stability
+color_echo "yellow" "Replacing Fedora Flatpak Repo with Flathub..."
+dnf install -y flatpak
+flatpak remote-delete fedora --force || true
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+sudo flatpak repair
+flatpak update
+
+# Enable RPM Fusion repositories to access additional software packages and codecs
+color_echo "yellow" "Enabling RPM Fusion repositories..."
+dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+dnf install -y https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+dnf update @core -y
+
+# Install multimedia codecs to enhance multimedia capabilities
+color_echo "yellow" "Installing multimedia codecs..."
+dnf swap ffmpeg-free ffmpeg --allowerasing -y
+dnf update @multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin -y
+dnf update @sound-and-video -y
+color_echo "green" "Done installing multimedia codecs"
+
+# Install Hardware Accelerated Codecs for AMD GPUs. This improves video playback and encoding performance on systems with AMD graphics.
+color_echo "yellow" "Installing AMD Hardware Accelerated Codecs..."
+dnf swap mesa-va-drivers mesa-va-drivers-freeworld -y
+dnf swap mesa-vdpau-drivers mesa-vdpau-drivers-freeworld -y
+
+sudo dnf copr enable wezfurlong/wezterm-nightly
+dnf_apps=(
+    @virtualization
+    discord
+    qbittorrent
+    wezterm
+)
+color_echo "yellow" "Installing  apps ${dnf_apps[@]}"
+dnf install -y ${dnf_apps[@]}
+color_echo "green" "Done installing ${dnf_apps[@]}"
+
+flatpak_apps=(
+    com.github.tchx84.Flatseal
+    net.davidotek.pupgui2
+    it.mijorus.gearlever
+)
+for app_id in "${flatpak_apps[@]}"; do
+    color_echo "yellow" "Installing $app_id"
+    flatpak install flathub "$app_id" -y
+    color_echo "green" "Done installing $app_id"
+done
