3269ef2edc3339fb4282b4f0e29c0bdb07e20200
Kelvin <chuakid@gmail.com>
2025-11-06 14:24:00 +0800

    update keymaps

--- common/.config/wezterm/wezterm.lua
+++ common/.config/wezterm/wezterm.lua
@@ -69,7 +69,10 @@ config.keys = {
 local smart_splits = wezterm.plugin.require("https://github.com/mrjones2014/smart-splits.nvim")
 smart_splits.apply_to_config(config, {
 	-- directional keys to use in order of: left, down, up, right
-	direction_keys = { "h", "j", "k", "l" },
+	direction_keys = {
+		move = { "h", "j", "k", "l" },
+		resize = { "LeftArrow", "DownArrow", "UpArrow", "RightArrow" },
+	},
 	-- modifier keys to combine with direction_keys
 	modifiers = {
 		move = "CTRL", -- modifier to use for pane movement, e.g. CTRL+h to move left
