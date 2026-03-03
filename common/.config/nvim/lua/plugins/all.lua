49295eac677a248c09e8f1710566a98754ee8bb8
Kelvin <chuakid@gmail.com>
2026-03-03 10:14:27 +0800

    remove ai

--- common/.config/nvim/lua/plugins/all.lua
+++ common/.config/nvim/lua/plugins/all.lua
@@ -2,29 +2,6 @@ return {
   -- ============================================================
   -- AI / Completion
   -- ============================================================
-  {
-    'milanglacier/minuet-ai.nvim',
-    config = function()
-      require('minuet').setup {
-        provider = 'openai_fim_compatible',
-        notify = false,
-        n_completions = 1,
-        context_window = 512,
-        provider_options = {
-          openai_fim_compatible = {
-            api_key = 'TERM',
-            name = 'Ollama',
-            end_point = 'http://localhost:11434/v1/completions',
-            model = 'qwen2.5-coder:7b',
-            optional = {
-              max_tokens = 56,
-              top_p = 0.9,
-            },
-          },
-        },
-      }
-    end,
-  },
   { -- Autocompletion
     'saghen/blink.cmp',
     version = '1.*',
