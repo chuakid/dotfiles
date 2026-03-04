311140a9225739d54dae02e6e7b1b1a5cc33e7a5
Kelvin <chuakid@gmail.com>
2026-03-04 22:44:27 +0800

    fix treesitter

--- common/.config/nvim/lua/plugins/all.lua
+++ common/.config/nvim/lua/plugins/all.lua
@@ -108,19 +108,11 @@ return {
   -- ============================================================
   -- Treesitter
   -- ============================================================
-  { -- Highlight, edit, and navigate code
+  {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
-    main = 'nvim-treesitter.config',
-    opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
-      auto_install = true,
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = { 'ruby' },
-      },
-      indent = { enable = true, disable = { 'ruby' } },
-    },
+    lazy = false,
+    opts = {},
   },
   {
     'nvim-treesitter/nvim-treesitter-context',
