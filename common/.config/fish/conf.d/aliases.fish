cafe427b588839c9993b5a422902b0c8b082e1e0
Kelvin <chuakid@gmail.com>
2026-03-04 23:02:23 +0800

    fix glog

--- common/.config/fish/conf.d/aliases.fish
+++ common/.config/fish/conf.d/aliases.fish
@@ -23,8 +23,7 @@ abbr --add gp 'git push'
 abbr --add gs 'git status'
 abbr --add gsc 'git status --show-stash'
 abbr --add gf 'git fetch --all'
-abbr --add gl 'git log --graph --abbrev-commit --decorate --format=format:\"%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)\" --all'
-
+abbr --add glog 'git log --graph --abbrev-commit --decorate --format=format:"%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)" --all'
 
 abbr --add title 'wezterm cli set-tab-title'
 abbr --add n nvim
