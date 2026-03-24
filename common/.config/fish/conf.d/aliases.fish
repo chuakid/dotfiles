7b29368acda311ea620e0f8a2c4921e42ed6468c
Kelvin <chuakid@gmail.com>
2026-03-24 10:52:33 +0800

    update aliases for wezterm cli

--- common/.config/fish/conf.d/aliases.fish
+++ common/.config/fish/conf.d/aliases.fish
@@ -25,7 +25,10 @@ abbr --add gsc 'git status --show-stash'
 abbr --add gf 'git fetch --all'
 abbr --add glog 'git log --graph --abbrev-commit --decorate --format=format:"%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)" --all'
 
-abbr --add title 'wezterm cli set-tab-title'
+# Wezterm
+abbr --add tabtitle 'wezterm cli set-tab-title'
+abbr --add workspacetitle 'wezterm cli rename-workspace'
+
 abbr --add n nvim
 
 abbr --add k kubectl
