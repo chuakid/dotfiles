36975abafcd04d8bd7fc3ab06a2b61310b11734e
Kelvin <chuakid@gmail.com>
2026-03-24 13:56:44 +0800

    remove unused

--- common/.config/wezterm/wezterm.lua
+++ common/.config/wezterm/wezterm.lua
@@ -31,22 +31,6 @@ resurrect.state_manager.periodic_save({
 	save_workspaces = true,
 })
 wezterm.on("gui-startup", resurrect.state_manager.resurrect_on_gui_startup)
--- loads the state whenever I create a new workspace
-wezterm.on("smart_workspace_switcher.workspace_switcher.created", function(window, path, label)
-	local workspace_state = resurrect.workspace_state
-	workspace_state.restore_workspace(resurrect.state_manager.load_state(label, "workspace"), {
-		window = window,
-		relative = true,
-		restore_text = true,
-		on_pane_restore = resurrect.tab_state.default_on_pane_restore,
-	})
-end)
-
--- Saves the state whenever I select a workspace
-wezterm.on("smart_workspace_switcher.workspace_switcher.selected", function(window, path, label)
-	local workspace_state = resurrect.workspace_state
-	resurrect.state_manager.save_state(workspace_state.get_workspace_state())
-end)
 
 -- keys
 config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 2000 }
